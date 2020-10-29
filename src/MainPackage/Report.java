b33cf240b9433fb26ea5dafa10062d52b7047b22
Carlos Dubón <carlos.dubon@outlook.es>
2020-10-29 10:06:24 -0600

    :3 hi

--- src/MainPackage/Report.java
+++ src/MainPackage/Report.java
@@ -36,37 +36,42 @@ public class Report {
 
     public ArrayList<String> getReport() {
         report.clear();
+        report.add("La clase de nombre "+getClassData().get(2).toString()+" se declaro en la linea "+getClassData().get(0).toString());
+        report.add("El modificador de accesso de la clase "+getClassData().get(2).toString()+" es "+getClassData().get(1).toString());
+        
         return report;
     }
 
     public ArrayList<Object> getClassData() {
         int lineIndexOfClassName = -1;
         String className = "";
+        String classType = "public";
 
         ArrayList<Object> data = new ArrayList();
-        
+
         for (int i = 0; i < fileLines.size(); i++) {
-            if (fileLines.get(i).equals("class")) {
+            if (fileLines.get(i).contains("class")) {
                 lineIndexOfClassName = i;
-                
-                String[] ignoreThis = {"public", "class"};
-                className = getName(fileLines.get(i),ignoreThis);
+
+                String[] ignoreThis = {"public", "class", "{"};
+                className = ignoreCharSequence(fileLines.get(i), ignoreThis);
             }
         }
-        
+
         data.add(lineIndexOfClassName);
+        data.add(classType);
         data.add(className);
-        
+
         return data;
     }
 
-    public String getName(String w, String[] ignore) {
+    public String ignoreCharSequence(String w, String[] ignore) {
         String word = "";
-        for(String wordToReplace : ignore){
+        for (String wordToReplace : ignore) {
             w = w.replace(wordToReplace, " ");
         }
         for (int i = 0; i < w.length(); i++) {
-            if (w.charAt(i) != ' ' && w.charAt(i) != '{') {
+            if(w.charAt(i) != ' '){
                 word += w.charAt(i);
             }
         }
